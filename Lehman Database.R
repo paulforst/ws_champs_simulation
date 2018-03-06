3b286336b3abd7a8fd53090d3edc8e186ea13206
Nathan Thomas <nthomas2@bellarmine.edu>
2018-03-05 23:28:04 -0500

    Adding data

---
+++ Lehman Database.R
@@ -0,0 +1,18 @@
+# Data being pulled from The Lehman Database
+
+library(readr)
+Batting <- read_csv("baseballdatabank-2017.1/core/Batting.csv")
+Pitching <- read_csv("baseballdatabank-2017.1/core/Pitching.csv")
+Master <- read_csv("baseballdatabank-2017.1/core/Master.csv")
+
+
+
+
+
+
+
+
+
+
+
+
